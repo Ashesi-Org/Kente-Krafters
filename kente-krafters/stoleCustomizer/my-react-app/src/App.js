7db4578aa046006e2809cf0cf92d7b94ac74cab0
Emmanuella <105435095+OD2022@users.noreply.github.com>
2023-12-06 18:42:50 +0000

    Integrated Apparel UI with React
    Successfully integrated Apparel UI package

---
+++ kente-krafters/stoleCustomizer/my-react-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import CreativeEditorSDKComponent from './CreativeEditorSDK'; // Adjust the import path
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Stole Customizer</h1>
+      <CreativeEditorSDKComponent />
+    </div>
+  );
+}
+
+export default App;
