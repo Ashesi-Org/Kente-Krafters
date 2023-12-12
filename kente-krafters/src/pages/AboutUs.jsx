025912133fc2eef7d9c4297bdbbd0222ead25380
Nice Cailie Ineza <91422726+Nicailie@users.noreply.github.com>
2023-12-12 20:06:57 +0000

    Update AboutUs.jsx

--- kente-krafters/src/pages/AboutUs.jsx
+++ kente-krafters/src/pages/AboutUs.jsx
@@ -1,29 +1,133 @@
-import React from 'react';
-import { PRODUCTS } from "../utils/data";
+// import React from 'react';
+// import { PRODUCTS } from '../utils/data';
+// import aboutImage from '../assets/carousel_img_2.jpg'; // Import a relevant image
 
+// const AboutUs = () => {
+//   return (
+//     <div style={styles.container}>
+//       <h2 style={styles.heading}>About Us</h2>
+//       <div style={styles.content}>
+//         <img
+//           src={aboutImage}
+//           alt="About Us"
+//           style={styles.image}
+//         />
+//         <p style={styles.text}>
+//           Welcome to our company! We are a passionate team dedicated to providing
+//           high-quality products/services.
+//         </p>
+//         <p style={styles.text}>
+//           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+//           tempor incididunt ut labore et dolore magna aliqua.
+//         </p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// const styles = {
+//   container: {
+//     maxWidth: '800px',
+//     margin: '0 auto',
+//     padding: '40px',
+//     textAlign: 'center',
+//     backgroundColor: '#f9f9f9',
+//     borderRadius: '10px',
+//     boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
+//   },
+//   heading: {
+//     fontSize: '2.5rem',
+//     marginBottom: '20px',
+//     color: '#333',
+//   },
+//   content: {
+//     display: 'flex',
+//     flexDirection: 'column',
+//     alignItems: 'center',
+//   },
+//   image: {
+//     width: '100%',
+//     maxWidth: '600px',
+//     borderRadius: '8px',
+//     marginBottom: '20px',
+//   },
+//   text: {
+//     fontSize: '1.1rem',
+//     lineHeight: '1.6',
+//     color: '#555',
+//     marginBottom: '15px',
+//   },
+// };
+
+// export default AboutUs;
+
+
+
+import React from 'react';
+import { PRODUCTS } from '../utils/data';
+import aboutImage from '../assets/carousel_img_2.jpg'; // Import a relevant image
 
 const AboutUs = () => {
   return (
     <div style={styles.container}>
-      <h2>About Us</h2>
-      <p>
-        Welcome to our company! We are a passionate team dedicated to providing
-        high-quality products/services.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
-      </p>
+      <div style={styles.overlay}>
+        <h2 style={styles.heading}>About Us</h2>
+        <div style={styles.content}>
+          <p style={styles.text}>
+          Family
+As a family enterprise that has been sustained three generations down, it has been an undulating journey. For just like the kente fabric in itself, with time comes growth and with this growth befalls struggle. In the recent past, this enterprise had been threatened by fires which sought to end the three generation-long business. 
+
+However, we at Woven present an answer to ensuring the sustenance that was started by our forefathers, by taking kente digital and providing a widened market for it; both in Ghana and across the continent. 
+
+With you in mind, we extend an invitation to join our family and take this journey of growth and sustenance with us, by being our loyal customers.
+          </p>
+          <p style={styles.text}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
 
 const styles = {
   container: {
-    maxWidth: '600px',
-    margin: '0 auto',
-    padding: '20px',
+    position: 'relative',
+    height: '100vh',
+    backgroundImage: `url(${aboutImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: '#fff',
+  },
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0, 0.65)',
+    borderRadius: '10px',
+    padding: '40px',
+   
+    maxWidth: '800px',
+    width: '100%',
+    boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
+  },
+  heading: {
+    fontSize: '2.5rem',
     textAlign: 'center',
+    marginBottom: '20px',
+    color: '#fff',
+  },
+  content: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: '1.1rem',
+    lineHeight: '1.6',
+    color: '#fff',
+    marginBottom: '15px',
   },
 };
 
